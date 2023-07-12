3c8c0e66219e5e3edc774edcc8ee8ea7968b4c1c
trlmtr <tugrulmatras@gmail.com>
2023-07-12 22:34:17 +0100

    second

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-        package='demo_node_cpp',
+        package='demo_nodes_cpp',
         executable='talker'
         )
     ])
